192a2c08c90f7b0aa0521b6888dbff6b768dfb20
Siddu-009 <siddardha9988@gmail.com>
2025-09-03 21:07:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-region = "ap-south-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
-ami = "ami-0f918f7e67a3323f0"
-instance_type = "t2.medium"
-key_name = "RMUMBAIKP"
-vpc_security_group_ids = ["sg-0da91ed9997d755fc"]
+count = 4
+ami = "ami-0360c520857e3138f"
+instance_type = "t3.micro"
+key_name = "siddu"
+vpc_security_group_ids = ["sg-01c91cd3a6719927b"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
+default = ["jenkins", "s3", "AppServer-1", "AppServer-2", "Monitoring server"]
 }
