447f392a5a859f56a9e90bcefd5b428152e62563
Siddu-009 <siddardha9988@gmail.com>
2025-09-03 22:47:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 variable "aws_region" {
   default = "us-east-1"
 }
+
 
 resource "aws_instance" "one" {
 count = 4
