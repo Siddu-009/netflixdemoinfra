169227cac3c78177a5bcae38db2e415afbc8b8da
Siddu-009 <siddardha9988@gmail.com>
2025-09-04 17:47:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,19 @@
 provider "aws" {
-  region = var.aws_region
-}
-
-variable "aws_region" {
-  default = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
-ami = "ami-0360c520857e3138f"
-instance_type = "t3.micro"
-key_name = "siddu"
-vpc_security_group_ids = ["sg-01c91cd3a6719927b"]
-tags = {
-Name = var.instance_names[count.index]
-}
+  count                  = 4
+  ami                    = "ami-0360c520857e3138f"
+  instance_type          = "t2.medium"
+  key_name               = "siddu"
+  vpc_security_group_ids = ["sg-01c91cd3a6719927b"]
+  tags = {
+    Name = var.instance_names[count.index]
+  }
 }
 
 variable "instance_names" {
-default = ["jenkins", "s3", "AppServer-1", "AppServer-2", "Monitoring server"]
+  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
+
